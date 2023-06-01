b9c187ae2ba1e06e065a1e9de24f40e96a39bf8f
KhondokarAlMaruf <khondokaralmaruf01@gmail.com>
2023-06-01 23:22:34 +0600

    set environment variable

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -4,14 +4,17 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+console.log(import.meta.env.VITE_apiKey);
 const firebaseConfig = {
-  apiKey: "AIzaSyA1ZYp9oLKfRWeUhNZGYTvLcjLDAEHi4Do",
-  authDomain: "the-foodies-a39b8.firebaseapp.com",
-  projectId: "the-foodies-a39b8",
-  storageBucket: "the-foodies-a39b8.appspot.com",
-  messagingSenderId: "459474125831",
-  appId: "1:459474125831:web:16ae457a52b96fa2ea96a2",
+  apiKey: import.meta.env.VITE_apiKey,
+
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
+console.log(firebaseConfig);
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
